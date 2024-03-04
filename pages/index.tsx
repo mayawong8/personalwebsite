f1608f786add74ea3984c67e77e2d81e1ae88ca5
Maya <mayajwong8@gmail.com>
2024-03-04 12:53:57 -0500

    started intro page

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,9 +14,14 @@ export default function Home() {
     <main>
       <section className="background-section-1 flex justify-center items-center h-screen">
         <div>
-          <h1 className="typewriter">Hi, I'm Maya Wong</h1>
+          <h1 className="typewriter font-mono">Hi, I'm Maya Wong</h1>
         </div>
-        
+      </section>
+      <section className="background-section-2 h-screen">
+        <div>
+          <p className="font-mono text-white text-4xl p-20">I graduated in Computer Science Honours with a concentration in Cybersecurity and a double minor in Business and Earth Sciences. </p>
+          <p className="font-mono text-white text-4xl p-20">I have interests in Cybersecurity, App/Web Development, and Sports. </p>
+        </div>
       </section>
     </main>
     </div>
